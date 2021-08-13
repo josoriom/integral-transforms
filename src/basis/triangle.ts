cb8e3b7005a59e2ff03157d75ff592de76dfd00a
WargnerM <wargner.moreno@correounivalle.edu.co>
2021-08-13 16:16:23 -0500

    feat: adding height as an optional parameter, fixed boolean operator in triangular function

--- src/basis/triangle.ts
+++ src/basis/triangle.ts
@@ -1,14 +1,16 @@
-function triangular(x: number, center: number, options: Options ={}){
-    const { width = 1 } = options;
-    if (x >= center - 0.5*width || x <= center) {
-        return 2*(x - center)/width +1;
-    } else if (x > center || x <= center + 0.5*width) {
-        return -2 * (x / width) + 2 * (center / width) + 1;
-    } else {
-        return 0;
+function triangular(x: number, options: Options ={}){
+    const { center = 1, width = 0.1, height = 1 } = options;
+    let result: number = 0;
+    if (x >= center - 0.5*width && x <= center) {
+        result = 2*height*(x - center)/width + height;
+    } else if (x > center && x <= center + 0.5*width) {
+        result = -2*height*(x - center)/width + height;
     }
+    return result;
 }
 
 interface Options {
+    center?: number
     width?: number
+    height?: number 
 }
