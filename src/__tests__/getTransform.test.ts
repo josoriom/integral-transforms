6a81b4bcabc9946da593fdc459837a2ccb1bd9ec
josoriom <javier.osorio@correounivalle.edu.co>
2021-08-20 12:16:45 -0500

    fix: move types to src folder

--- src/__tests__/getTransform.test.ts
+++ src/__tests__/getTransform.test.ts
@@ -13,8 +13,9 @@ describe('Testing basis functions', () => {
     const transform = getTransform(input);
 
     expect(transform).toBeDeepCloseTo(
-      [0.14, 0.25, 0.37, 0.49, 0.62, 0.74, 0.86, 0.96, 1, 0.96, 0.86, 0.74, 0.62,
-       0.49, 0.37, 0.25, 0.14,
+      [
+        0.14, 0.25, 0.37, 0.49, 0.62, 0.74, 0.86, 0.96, 1, 0.96, 0.86, 0.74,
+        0.62, 0.49, 0.37, 0.25, 0.14,
       ],
       2,
     );
