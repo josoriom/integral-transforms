45edecdb8bd125cb9b8cfefa27e238fda4540b06
josoriom <javier.osorio@correounivalle.edu.co>
2021-08-20 12:24:32 -0500

    fix: basis test case

--- src/basis/__tests__/basis.test.ts
+++ src/basis/__tests__/basis.test.ts
@@ -1,26 +1,30 @@
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
+
 import { gaussian } from '../gaussian';
 import { lorentzian } from '../lorentzian';
 import { triangular } from '../triangular';
 
+expect.extend({ toBeDeepCloseTo });
+
 describe('Testing basis functions', () => {
   it('Triaungular', () => {
-    expect(triangular(11)).toStrictEqual([
-      0, 0.19999999999999996, 0.4, 0.6, 0.8, 1, 0.8, 0.6000000000000001,
-      0.3999999999999999, 0.19999999999999996, 0,
-    ]);
+    expect(triangular(11)).toBeDeepCloseTo(
+      [0, 0.2, 0.4, 0.6, 0.8, 1, 0.8, 0.6, 0.4, 0.2, 0],
+      1,
+    );
   });
 
   it('Gaussian', () => {
-    expect(gaussian(11)).toStrictEqual([
-      0, 0.19999999999999996, 0.4, 0.6, 0.8, 1, 0.8, 0.6000000000000001,
-      0.3999999999999999, 0.19999999999999996, 0,
-    ]);
+    expect(gaussian(11)).toBeDeepCloseTo(
+      [0, 0.2, 0.4, 0.6, 0.8, 1, 0.8, 0.6, 0.4, 0.2, 0],
+      1,
+    );
   });
 
   it('Lorentzian', () => {
-    expect(lorentzian(11)).toStrictEqual([
-      0, 0.19999999999999996, 0.4, 0.6, 0.8, 1, 0.8, 0.6000000000000001,
-      0.3999999999999999, 0.19999999999999996, 0,
-    ]);
+    expect(lorentzian(11)).toBeDeepCloseTo(
+      [0, 0.2, 0.4, 0.6, 0.8, 1, 0.8, 0.6, 0.4, 0.2, 0],
+      1,
+    );
   });
 });
